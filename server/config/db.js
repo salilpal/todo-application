e3fdd1fd7845d08f811a0317c64f07bb56739f03
salilpal <salilp379@gmail.com>
2026-01-28 00:55:09 +0530

    created a basic todo server

--- server/config/db.js
+++ server/config/db.js
@@ -4,3 +4,15 @@ mongoose
     .connect(process.env.mongodbURI)
     .then(() => console.log('mongodb connected'))
     .catch((e) => console.error(`error connecting mongodb ${e.message}`))
+
+const TodoSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    completed: Boolean
+})
+
+const Todo = mongoose.model('Todo', TodoSchema)
+
+module.exports = {
+    Todo
+}
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const todoRoutes = require('./routes/todoRoutes')
+
 const app = express()
+
 const port = process.env.port
 
 app.use(express.json())
+app.use('/todos', todoRoutes)
 
 app.listen(port, () => {
     console.log(`app is listening to port ${port}`)
